b547ed0d1f4c8b358400e25b4445be895d06b891
Kris Janssen <kris.janssen@gmail.com>
2014-11-20 11:39:29 +0100

    Fixed a menu duplication issue upon successive runs of the app

--- core/gui/menu/addmainmenu.m
+++ core/gui/menu/addmainmenu.m
@@ -2,17 +2,28 @@ function addmainmenu( hfig )
 %ADDMENU Summary of this function goes here
 %   Detailed explanation goes here
 
-    % Get the main app window and the handles
-    %hfig = getappdata(0, 'mainWindow');
-    handles = guidata(hfig);
-    
-    % Build the menu
-    FileMenu = uimenu( hfig, 'Label', 'File' );
-    uimenu( FileMenu, 'Label', 'Open', 'Callback', @onFileOpen );
-    uimenu( FileMenu, 'Label', 'Exit', 'Callback', @onExit );
-    
-    % Store the handles to the new menu
-    handles.filemenu = FileMenu;
-    guidata(hfig, handles);
+% Get the main app window and the handles
+%hfig = getappdata(0, 'mainWindow');
+handles = guidata(hfig);
+% 
+% if exist('handles.filemenu','var') %if it's there, delete it.
+%     delete(mh);
+% end
+
+% Build the menu
+
+% Make sure the menus are not already there...
+existingmenus = findall( hfig, 'Type', 'uimenu' );
+
+arrayfun(@(x) delete(x), existingmenus);
+
+FileMenu = uimenu( hfig, 'Label', 'File' );
+uimenu( FileMenu, 'Label', 'Open CL', 'Callback', @onFileOpen );
+uimenu( FileMenu, 'Label', 'Open SEM', 'Callback', @onFileOpen );
+uimenu( FileMenu, 'Label', 'Exit', 'Callback', @onExit );
+
+% Store the handles to the new menu
+handles.filemenu = FileMenu;
+guidata(hfig, handles);
 
 end
