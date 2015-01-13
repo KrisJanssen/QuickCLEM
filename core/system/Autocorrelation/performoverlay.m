39d5e8215edf4a329e2b4b8594caa99699e9c177
Kris Janssen <kris.janssen@gmail.com>
2015-01-13 01:04:05 +0100

    Corrected erros in overlay generation.

--- core/system/Autocorrelation/performoverlay.m
+++ core/system/Autocorrelation/performoverlay.m
@@ -3,8 +3,9 @@ function performoverlay( transformedPoints, imageSEMO, infoSEMO, imageSEMZ, info
 %   Parameters:
 %   
 %   transformedPoints:  The XY coordinates of events to overlay expressed
-%                       as pixel coordinates in the 768 by 512 raster that 
-%                       constitutes the SEM image.
+%                       as pixel coordinates in the 512 * 512 raster that 
+%                       constitutes the central part of the SEM image as 
+%                       scanned.
 %   imageSEMO:          The SEM overview image
 %   infoSEMO:           Info on the SEM overview image
 %   imageSEMZ:          The SEM zoom image
@@ -25,12 +26,15 @@ overlayedSEM = subplot(1,2,1);
 
 % Get the dimensions of the SEM image in pixels, here, the annotation bar
 % is included!
-XWidth = infoSEMO.Width;
-YWidth = infoSEMO.Height;
+SEMImageWidth = infoSEMO.Width;
+SEMImageHeight = infoSEMO.Height;
 
 % The image tif contains metadata that is non-standard. From it, we get the
 % width of a pixel in meters. We start looking for it at the 'PixelWidth='
 % tag and we use a regex to get the number.
+%
+% Note that, because of the way the SEM records images, pixel width = pixel
+% height.
 %
 % Optionally a - or a +: (\-|\+)?
 % Any number of digits, a dot and again any number of digits: \d+\.\d+
@@ -39,29 +43,32 @@ YWidth = infoSEMO.Height;
 % Any number of digits: \d+
 regex = 'PixelWidth\=(\-|\+)?\d+\.\d+e(\-|\+)?\d+';
 
-rawPixelWidtO = infoSEMO.UnknownTags.Value;
-rawPixelWidtZ = infoSEMZ.UnknownTags.Value;
+rawPixelWidthO = infoSEMO.UnknownTags.Value;
+rawPixelWidthZ = infoSEMZ.UnknownTags.Value;
 
 pixelWidthO = strrep( ...
-    regexp(rawPixelWidtO,regex,'match'), ...
+    regexp(rawPixelWidthO,regex,'match'), ...
     'PixelWidth=', ...
     '');
 
 pixelWidthZ = strrep( ...
-    regexp(rawPixelWidtZ,regex,'match'), ...
+    regexp(rawPixelWidthZ,regex,'match'), ...
     'PixelWidth=', ...
     '');
 
-% Convert to a numeric value.
+% Convert to a numeric value. Again this value is identical for X and Y.
 PixelResolutionO = str2double(pixelWidthO(1));
 PixelResolutionZ = str2double(pixelWidthZ(1));
 
 % SEM scans images as 768 * 512 raster. The tiff is thus oversampled.
-OverSample = XWidth / 768;
+OverSample = SEMImageWidth / 768;
 
-% Physical width of the image in m.
-HFWO = XWidth * PixelResolutionO;
-HFWZ = XWidth * PixelResolutionZ;
+% Physical width of the image in m. Because the pixel size is the same in X
+% and Y and because no. of pixels in X is higher than in Y (rectangluar
+% images instead of square) the field of view in X and Y is of course
+% different in size.
+HFWO = SEMImageWidth * PixelResolutionO;
+HFWZ = SEMImageWidth * PixelResolutionZ;
 
 % The CL spots are layed out around the ROI so we have an overview image 
 % and the actual detail image. We need the scale factor between both
@@ -71,25 +78,27 @@ ZoomFactor = HFWO / HFWZ;
 % Since the SEM scans a 768 * 512 raster and we know the oversampling
 % factor from the X dimension, we can work out the width of the annotation
 % band on the SEM output and get rid of it.
-YWidthReal = OverSample * 512;
+SEMImageHeightCropped = OverSample * 512;
 
 % Crop only the actual image.
 imageSEMOCropped = imageSEMO( ...
-    1:YWidthReal , ...
-    1:XWidth );
+    1:SEMImageHeightCropped , ...
+    1:SEMImageWidth );
 
 imageSEMZCropped = imageSEMZ( ...
-    1:YWidthReal , ...
-    1:XWidth );
+    1:SEMImageHeightCropped , ...
+    1:SEMImageWidth );
 
 % Paint this image.
 imshow( imadjust( imageSEMOCropped ) );
 
 axis image;
 
-% Transform raster (768 * 512) coordinates to image pixel coordinates.
+% Transform raster 512 * 512 (row * col) coordinates to SEM image pixel 
+% coordinates.
 % We also need to shift by 128 in X since the EM-CCD is 512 * 512 so there
-% wille be 128 borders on each side.
+% will be 128 borders on each side because the SEM actually records on a
+% 768 * 512 grid.
 % Of course, we also correct for the oversampling of the actual TIFF.
 pointsScaled(:,1) = ((transformedPoints(:,1) + 128) * OverSample);
 pointsScaled(:,2) = ((transformedPoints(:,2)) * OverSample);
@@ -106,10 +115,10 @@ pointsScaledZoom = pointsScaled;
 % We also apply the X and Y shift here.
 pointsScaledZoom(:,1) = ...
     ( ShiftX / PixelResolutionZ ) + ...
-    ( ( pointsScaled(:,1) - ( XWidth / 2 ) ) * ZoomFactor ) + ( XWidth / 2 );
+    ( ( pointsScaled(:,1) - ( SEMImageWidth / 2 ) ) * ZoomFactor ) + ( SEMImageWidth / 2 );
 pointsScaledZoom(:,2) = ...
     ( ShiftY / PixelResolutionZ ) + ...
-    ( ( pointsScaled(:,2) - ( YWidthReal / 2 ) ) * ZoomFactor ) + ( YWidthReal / 2 );
+    ( ( pointsScaled(:,2) - ( SEMImageHeightCropped / 2 ) ) * ZoomFactor ) + ( SEMImageHeightCropped / 2 );
 
 % Add the localized events on top of the image.
 hold all
@@ -138,38 +147,36 @@ hold all
 % For binning, we can use hist3
 % First, convert binsize to a discrete number of bins. We do so by checking
 % how many times the desired bin width fits into the physical size of the
-% image. We do this in the Y direction. The X direction is actually the
-% same.
-nbins = round( PixelResolutionZ * YWidthReal / BinWidth );
+% image. We do this in the X direction.
+nbinsX = round( PixelResolutionZ * SEMImageWidth / BinWidth );
+nbinsY = round( PixelResolutionZ * SEMImageHeightCropped / BinWidth );
 
 % Based on the size of a bin, we calculate the location of bin edges.
-edges = linspace(1, nbins, nbins) * (BinWidth / PixelResolutionZ);
+edgesX = linspace(1, nbinsX, nbinsX) * (BinWidth / PixelResolutionZ);
+edgesY = linspace(1, nbinsY, nbinsY) * (BinWidth / PixelResolutionZ);
 
 % Store these edges in a format hist3 likes.
 edgesxy = cell(1,2);
-edgesxy{1} = edges;
-edgesxy{2} = edges;
+edgesxy{1} = edgesX;
+edgesxy{2} = edgesY;
 
 % Generate the actual 2D histogram. N is the 'bitmap' holding the counts
 % for each bin. We will take this bitmap and blow it up to be the same size
 % (in px) as the SEM image so that we can overlay easily.
 [ N, C ] = hist3(pointsScaledZoom, 'Edges', edgesxy);
 
-% It is easiest to blow up the low res histogram, which is basically a 
-% bitmap to be the same size as the EM image.
-blowupfactor = YWidthReal / size(N, 1);
-
-% The actual blowing up.
-squareHeatmap = imresize(transpose(N),blowupfactor,'nearest');
-
-% We now create an array the size of the EM image.
-fullHeatmap = zeros(YWidthReal, XWidth);
-
-% We place the blown up histogram in the middle of this map.
-fullHeatmap(:,( 128 * OverSample ) + 1:( 128 * OverSample ) + YWidthReal) = squareHeatmap;
+% The SEM generated tif files are 512 * 768 aspect ratio where each raster 
+% point is oversampled. We thus need to convert our 2D histogram to be the
+% same size.
+% Additionally, hist3 somehow generates transposed output, we need to
+% compensate for that.
+upscaledHeatmap = imresize(...
+    transpose(N), ...
+    [SEMImageHeightCropped, SEMImageWidth], ...
+    'nearest');
 
 % Show the overlayed figure.
-c = overlayImg(imageSEMZCropped, fullHeatmap); 
+c = overlayImg(imageSEMZCropped, upscaledHeatmap); 
 
 % We want the regions with no events to be 100% transparent in the overlay.
 c.setTranspRange([0 0])
